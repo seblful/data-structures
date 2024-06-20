2a53daa25e9a92c922b72d15dd0c25b155c6bd06
seblful <alesha.chimba@gmail.com>
2024-06-20 11:37:43 +0300

    [ADD] init commit

---
+++ binary_heap.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct Heap {
+    int *arr;
+    int size;
+    int capacity;
+    
+} heap;
+
+
+int main() {
+
+    return 0;
+}
