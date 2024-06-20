23fbc25d6182ba726d3cfec4c05fbc562ff01f48
seblful <alesha.chimba@gmail.com>
2024-06-20 13:32:25 +0300

    [EDIT] binary tree

--- binary_tree.c
+++ binary_tree.c
@@ -7,3 +7,77 @@ struct node
     struct node *left;
     struct node *right;
 };
+void insert(struct node **root, int value)
+{
+    if (*root == NULL)
+    {
+        *root = (struct node *)malloc(sizeof(struct node));
+        (*root)->data = value;
+        (*root)->left = (*root)->right = NULL;
+    }
+    else if (value < (*root)->data)
+    {
+        insert(&(*root)->left, value);
+    }
+    else if (value > (*root)->data)
+    {
+        insert(&(*root)->right, value);
+    }
+}
+
+void inorder(struct node *root)
+{
+    if (root != NULL)
+    {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+void preorder(struct node *root)
+{
+    if (root != NULL)
+    {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(struct node *root)
+{
+    if (root != NULL)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+int main()
+{
+    struct node *root = NULL;
+
+    insert(&root, 5);
+    insert(&root, 3);
+    insert(&root, 7);
+    insert(&root, 2);
+    insert(&root, 4);
+    insert(&root, 6);
+    insert(&root, 8);
+
+    printf("Inorder Traversal: ");
+    inorder(root);
+    printf("\n");
+
+    printf("Preorder Traversal: ");
+    preorder(root);
+    printf("\n");
+
+    printf("Postorder Traversal: ");
+    postorder(root);
+    printf("\n");
+
+    return 0;
+}
